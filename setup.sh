3412e25eccbfd52b81c1556f2f774874b7188d67
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-31 21:33:59 +0000

    Added script setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,16 @@
 # First time
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "Setting up..."
+mv "$SCRIPT_DIR/dist/gitdist" "$SCRIPT_DIR/gitdist/gitdist"
+mv "$SCRIPT_DIR/gitdist" "/usr/local/gitdist"
+if [ $? != 0 ]; then
+  mv "$SCRIPT_DIR/gitdist" "$( dirname $SCRIPT_DIR )/gdist"
+  export PATH=$PATH:$( dirname $SCRIPT_DIR )/gdist
+  export PATH=$PATH:$( dirname $SCRIPT_DIR )/gdist/files
+else
+  export PATH=$PATH:/usr/local/gitdist
+  export PATH=$PATH:/usr/local/gitdist/files
+fi
+echo "Setup complete."
+rm -r -f $SCRIPT_DIR
+alias gdist="gitdist"
