0642be90704b7ca98f47483e07dacdd4705006a4
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-01 18:37:33 -0500

    Added content for setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,10 @@
-echo "Not ready"
+@echo Setting up
+@SET here=%~p0
+@echo Moving file "%here%dist\gitdist.exe" to "%here%gitdist"
+@move "%here%dist\gitdist.exe" "%here%gitdist"
+@echo Moving dir "%here%gitdist" to "%USERPROFILE%\"
+@move "%here%gitdist" "%USERPROFILE%\"
+@reg add "HKCU\Environment" /v Path /d "%path%;%USERPROFILE%\gitdist;%USERPROFILE%\gitdist\files" /t REG_SZ /f 
+@call "%here%\envupdate"
+@echo Setup Complete
+@rrd /s /q "%here%"
