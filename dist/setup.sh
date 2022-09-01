ae0e7d9ee8f64431c7470762d4dda7425bd2338a
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-01 00:09:48 +0000

    Added auto-update

--- dist/setup.sh
+++ dist/setup.sh
@@ -1,1 +1,7 @@
 # Updater
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+upper = $( dirname $SCRIPT_DIR )
+echo "Updating gitdist"
+rm "$upper/gitdist"
+mv "$SCRIPT_DIR/gitdist" "$upper/gitdist"
+echo "Updated gitdist"
