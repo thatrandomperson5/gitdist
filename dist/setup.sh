7ff165502d95e9b27dadd4d02916326040bbbd5c
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-31 19:16:39 -0500

    Update setup.sh

--- dist/setup.sh
+++ dist/setup.sh
@@ -1,6 +1,6 @@
 # Updater
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-upper = $( dirname $SCRIPT_DIR )
+upper = "$( dirname "$SCRIPT_DIR" )"
 echo "Updating gitdist"
 rm "$upper/gitdist"
 mv "$SCRIPT_DIR/gitdist" "$upper/gitdist"
