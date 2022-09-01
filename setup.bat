755041462d5802ce17b6c596bb5a4307bc9f200c
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-01 18:43:45 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,7 +4,7 @@
 @move "%here%dist\gitdist.exe" "%here%gitdist"
 @echo Moving dir "%here%gitdist" to "%USERPROFILE%\"
 @move "%here%gitdist" "%USERPROFILE%\"
-@reg add "HKCU\Environment" /v Path /d "%path%;%USERPROFILE%\gitdist;%USERPROFILE%\gitdist\files" /t REG_SZ /f 
+@reg add "HKCU\Environment" /v Path /d "%path%;%USERPROFILE%\gitdist\;%USERPROFILE%\gitdist\files\" /t REG_SZ /f 
 @call "%here%\envupdate"
 @echo Setup Complete
 @rd /s /q "%here%"
