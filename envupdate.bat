6e11372c805bdd704930be08f804e81d24e647f8
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-01 19:17:05 -0500

    Update envupdate.bat

--- envupdate.bat
+++ envupdate.bat
@@ -17,23 +17,6 @@ REM (at your option) any later version.
 REM see www.gnu.org/licenses
 
 
-REM ================= ABOUT THE PROGRAM =================
-REM This program is Created by anic17 at 'Wed 01/06/2021 - 19:25'
-REM This program can A plugin to update environment settings without rebooting.
-REM For More Visit: www.batch-man.com
-
-
-REM Setting version information...
-Set _ver=1.0
-
-
-REM Checking for various parameters of the function...
-If /i "%~1" == "/?" (goto :help)
-If /i "%~1" == "-h" (goto :help)
-If /i "%~1" == "-help" (goto :help)
-If /i "%~1" == "help" (goto :help)
-If /i "%~1" == "ver" (Echo.%_ver%&Goto :End)
-
 REM Starting Main Program...
 :Main
 (
@@ -64,28 +47,3 @@ Echo.[uintptr]::Zero, "Environment", 2, 5000, [ref]$result^);
 for /f %%a in ('powershell -ExecutionPolicy Bypass "!Temp!\EnvUpdate.ps1"') do (set "_Result=%%~a")
 if /i "%_Result%" == "0" (Echo. Changes not applied!&& Exit /b 1)
 Exit /b 0
-
-:Help
-Echo.
-Echo. A plugin to update environment settings without rebooting or logging off 
-echo. using WM_SETTINGCHANGE system call.
-Echo. CREDITS: EnvUpdate %_ver% by anic17
-Echo. CREDITS: http://web.archive.org/web/20170516120430/http://poshcode.org/2049
-echo.
-echo. Syntax: Call EnvUpdate
-echo. Syntax: call EnvUpdate [help , /? , -h , -help]
-echo. Syntax: call EnvUpdate ver
-echo.
-echo. Where:-
-echo.
-echo. ver		: Displays version of program
-echo. help		: Displays help for the program
-Echo.
-Echo. Example: Call EnvUpdate
-Echo. Example: Call EnvUpdate ver
-Echo. Example: Call EnvUpdate [/? , -h , -help , help]
-Echo.
-Echo.
-Echo. www.batch-man.com
-Echo. #batch-man
-Goto :EOF
